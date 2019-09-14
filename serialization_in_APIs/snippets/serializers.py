5f12522ea8aa8bc869a6496641e8e7d385bc65fb
mohamadaref <aliismyimam@gmail.com>
2019-09-14 23:27:18 +0430

    in this version I added the hyperlinkes and now the API is browsable simply by clicking on the links

--- serialization_in_APIs/snippets/serializers.py
+++ serialization_in_APIs/snippets/serializers.py
@@ -25,15 +25,16 @@ from django.contrib.auth.models import User
 #         return self.code
 
 
-class SnippetSerializer(serializers.ModelSerializer):
+class SnippetSerializer(serializers.HyperlinkedModelSerializer):
     owner = serializers.ReadOnlyField(source = 'owner.username')
+    highlight = serializers.HyperlinkedIdentityField(view_name='snippet-highlight' , format = 'html')
     class Meta:
         model = Snippet
-        fields = ['id' , 'title' , 'code' , 'owner' , 'linenos' , 'language' , 'style']
+        fields = ['url' , 'highlight' , 'id' , 'title' , 'code' , 'owner' , 'linenos' , 'language' , 'style']
 
 
-class UserSerializer(serializers.ModelSerializer):
-    snippets = serializers.PrimaryKeyRelatedField(many = True , queryset = Snippet.objects.all())
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    snippets = serializers.HyperlinkedRelatedField(many = True , view_name = 'snippet-detail' , read_only = True)
 
     class Meta:
         model = User
--- serialization_in_APIs/snippets/views.py
+++ serialization_in_APIs/snippets/views.py
@@ -6,6 +6,19 @@ from rest_framework import generics
 from django.contrib.auth.models import User
 from rest_framework import permissions
 from .permissions import IsOwnerOrReadOnly
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+from rest_framework import renderers
+
+
+@api_view(['GET'])
+def api_root(request , format = None):
+    return Response({
+        'users': reverse('user-list' , request = request , format = format),
+        'snippets': reverse('snippet-list' , request = request , format = format),
+    })
+
 
 class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
@@ -22,13 +35,22 @@ class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
 
     permission_classes = [permissions.IsAuthenticatedOrReadOnly , IsOwnerOrReadOnly]
 
+
 class UserList(generics.ListAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
 
 class UserDetail(generics.RetrieveAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
+class SnippetHighlight(generics.GenericAPIView):
+    queryset = Snippet.objects.all()
+    renderer_classes = [renderers.StaticHTMLRenderer]
+
+    def get(self , request , *args , **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
 
 # Create your views here.
